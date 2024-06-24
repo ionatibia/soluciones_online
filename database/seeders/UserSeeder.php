34aed1659f67c89d342c2923f5524b6e07155067
ionatibia <iker.onatibia@gmail.com>
2024-06-24 14:51:19 +0200

    checkpoint

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'id' => 1,
+            'name' => 'Super',
+            'surname' => 'Admin',
+            'username' => 'SuperAdmin',
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'password' => Hash::make('12345678')
+        ]);
+    }
+}
